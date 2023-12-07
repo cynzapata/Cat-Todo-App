9ce063cdbd2c69402329fd628e3570f02eca1b27
Cynthia Zapata <cynthiazapata2017@gmail.com>
2023-12-07 15:20:11 -0600

    added checkboxes to the app

--- to-do-app/src/Components/RandomQuote/RandomQuote.jsx
+++ to-do-app/src/Components/RandomQuote/RandomQuote.jsx
@@ -31,13 +31,19 @@ export const RandomQuote = () => {
       <div className='container'>
         <div className="title"><span className="font-link">TASKS FOR PRODUCTIVITY</span></div>
         <div className="task-subtitle"><span className="font-link">HIGHEST PURIORITY</span></div>
+        <input type="checkbox"/>
         <input type="text" placeholder="My task goes here"/>
+        <input type="checkbox"/>
         <input type="text" placeholder="My task goes here"/>
         <div className="task-subtitle"><span className="font-link">HIGHEST PURIORITY</span></div>
+        <input type="checkbox"/>
         <input type="text" placeholder="My task goes here"/>
+        <input type="checkbox"/>
         <input type="text" placeholder="My task goes here"/>
         <div className="task-subtitle"><span className="font-link">HIGHEST PURIORITY</span></div>
+        <input type="checkbox"/>
         <input type="text" placeholder="My task goes here"/>
+        <input type="checkbox"/>
         <input type="text" placeholder="My task goes here"/>
         <div className="quote-subtitle"><span className="font-link">LOOKING PUR MOTIVATION?</span></div>
         <div className="quote"><span className="font-link">{quote.quote}</span></div>
