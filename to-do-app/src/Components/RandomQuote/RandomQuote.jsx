38e2ff4a4ae345bd08c287923d24b596071e2d18
Cynthia Zapata <cynthiazapata2017@gmail.com>
2023-12-07 22:43:24 -0600

    added cat photos to quotes

--- to-do-app/src/Components/RandomQuote/RandomQuote.jsx
+++ to-do-app/src/Components/RandomQuote/RandomQuote.jsx
@@ -3,37 +3,41 @@ import './RandomQuote.css'
 
 
 export const RandomQuote = () => {
-    const [quote, setQuote] = useState({
-      quote: "\"You cannot find purr-peace by avoiding life, nya~.\" – Virginia Whiskers"
-    });
-  
-    async function loadQuotes() {
-      const response = await fetch("quotes.json");
-      const quotes = await response.json();
-      const select = quotes[Math.floor(Math.random() * quotes.length)];
-      setQuote(select);
-    }
-  
-    useEffect(() => {
-      // Load initial quote
-      loadQuotes();
-  
-      // Set an interval to load a new random quote every 30 minutes
-      const interval = setInterval(loadQuotes, 30 * 60 * 1000);
-  
-      // Cleanup the interval on component unmount
-      return () => {
-        clearInterval(interval);
-      };
-    }, []);
+  const [quote, setQuote] = useState({
+    quote: "\"You cannot find purr-peace by avoiding life, nya~.\" – Virginia Whiskers",
+    photo: "logo-512"
+  });
+
+  async function loadQuotes() {
+    const response = await fetch("quotes.json");
+    const quotes = await response.json();
+    const select = quotes[Math.floor(Math.random() * quotes.length)];
+    setQuote(select);
+  }
+
+  useEffect(() => {
+    // Load initial quote
+    loadQuotes();
+
+    // Set an interval to load a new random quote every 30 minutes
+    const interval = setInterval(loadQuotes, 30 * 60 * 1000);
+
+    // Cleanup the interval on component unmount
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
   
     return (
       <div className='container'>
         
         <div className="title"><span className="font-link">TASKS FOR PRODUCTIVITY</span></div>
         
+
         <div className='content-container'>
+
         <div className='inner-container'>
+          
         <div className="task-subtitle"><span className="font-link">HIGHEST PURIORITY</span></div>
         <label>
         <input type="checkbox" onChange={() => {}} />
@@ -67,7 +71,11 @@ export const RandomQuote = () => {
 
       <div className='inner-container'>
         <div className="quote-subtitle"><span className="font-link">LOOKING PUR MOTIVATION?</span></div>
-        <div className="quote"><span className="font-link">{quote.quote}</span></div>
+        <div className="quote">
+            <span className="font-link">{quote.quote}</span>
+            {/* Render the photo here */}
+            <img src={`${process.env.PUBLIC_URL}/images/${quote.photo}.png`} />
+          </div>
       </div>
       </div>
       </div>
